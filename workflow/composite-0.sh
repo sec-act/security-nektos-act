416b676ed4eb43323fef3fe50c71361bf3f204a5
koti reddy <kotireddya@kotis-MacBook-Pro.local>
2021-12-09 17:06:25 +0530

    update

--- workflow/composite-0.sh
+++
@@ -1,4 +0,0 @@
-echo "::group::Install Gobuster with go get"
-[ ! -x /home/runner/go/bin/gobuster ] && GO111MODULE=on go get -v github.com/OJ/gobuster/v3@latest
-echo "/home/runner/go/bin/" >> $GITHUB_PATH
-echo "::endgroup::"
